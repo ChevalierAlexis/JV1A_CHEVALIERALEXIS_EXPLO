76ff2a08eed5363164fd3acdb930f866485c3043
ChevalierAlexis <al.chevalier@rns.ecolescreatives.com>
2022-03-22 10:24:40 +0100

    Exercice1

--- main.cpp
+++ main.cpp
@@ -3,7 +3,20 @@
 
 #include "main.h"
 #include <iostream>
+#include <string>
 
+int choix;
 
+int main(){
+std::cout<<"Pour aller dans la forêt, tapez 1"<<std::endl;
+std::cout<<"Pour aller dans les marais, tapez 2"<<std::endl;
+std::cin >> choix;
+if (choix==1){
+    std::cout<<"Vous êtes dans la forêt"<<std::endl;
+}
+if (choix==2){
+    std::cout<<"Vous êtes dans les marais"<<std::endl;
+}
+}
 
 #endif
